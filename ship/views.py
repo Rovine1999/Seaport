c91ea13663ffee5dccd1060042106b6694e37725
costamay <titusouko@gmail.com>
2021-12-02 08:57:01 +0300

    Correct form details

--- ship/views.py
+++ ship/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 # Create your views here.
 
